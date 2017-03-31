6fe6893f6dc82feb75c4b9f3e189619ed098af15
lynchmaniac <lynchmaniac@gmail.com>
2017-03-31 16:46:50 +0200

    refactor ajout de fonction

--- python/arm_moves.py
+++ python/arm_moves.py
@@ -3,52 +3,64 @@
 from Adafruit_PWM_Servo_Driver import PWM
 import time
 
-# ===========================================================================
-# Example Code
-# ===========================================================================
-
 # Initialise the PWM device using the default address
 #pwm = PWM(0x40)
 # Note if you'd like more debug output you can instead run:
 pwm = PWM(0x40, debug=True)
 
-#servoMin = 150  # Min pulse length out of 4096
-#servoMax = 600  # Max pulse length out of 4096
+# HITEC HS-5645MG
+CHANNEL_LEFT_ARM = 0
+SERVO_MIN_LEFT_ARM = 1000  # Min pulse length out of 4096 POSITION BASSE
+SERVO_MIDDLE_LEFT_ARM = 1500  # Middle pulse length out of 4096
+SERVO_MAX_LEFT_ARM = 2900  # Max pulse length out of 4096 POSITION HAUTE
 
-#servoMin = 900  # Min pulse length out of 4096
-#servoMax = 2200  # Max pulse length out of 4096
+# TOWER PRO MG 90
+CHANNEL_LEFT_HAND = 1
+SERVO_MIN_LEFT_HAND = 150  # Min pulse length out of 4096 POSITION BASSE
+SERVO_MIDDLE_LEFT_HAND = 370  # Middle pulse length out of 4096
+SERVO_MAX_LEFT_HAND = 590  # Max pulse length out of 4096 POSITION HAUTE
 
-servoMin = 1000  # Min pulse length out of 4096 POSITION BASSE
-servoMiddle = 1500  # Middle pulse length out of 4096
-servoMax = 2900  # Max pulse length out of 4096 POSITION HAUTE
 
+#pwm.setPWMFreq(60)                        # Set frequency to 60 Hz pour TOWER PRO MG 90
+pwm.setPWMFreq(300)                        # Set frequency to 300 Hz pour le HITEC HG-5645MG
 
-def setServoPulse(channel, pulse):
-  print "coucouc"
-  pulseLength = 1000000                   # 1,000,000 us per second
-  pulseLength /= 120                       # 60 Hz
-  print "%d us per period" % pulseLength
-  pulseLength /= 4096                     # 12 bits of resolution
-  print "%d us per bit" % pulseLength
-  pulse *= 1000
-  pulse /= pulseLength
-  print pulse
-  pwm.setPWM(channel, 0, pulse)
+def upAndDownLeftArm(sleep = 2):
+  #pwm.setPWMFreq(300) 
+  pwm.setPWM(CHANNEL_LEFT_ARM, 0, SERVO_MIN_LEFT_ARM)
+  time.sleep(sleep)
+  pwm.setPWM(CHANNEL_LEFT_ARM, 0, SERVO_MAX_LEFT_ARM)
+  time.sleep(sleep)
+  pwm.setPWM(CHANNEL_LEFT_ARM, 0, SERVO_MIDDLE_LEFT_ARM)
+  time.sleep(sleep)
 
-pwm.setPWMFreq(300)                        # Set frequency to 60 Hz
+def leftAndRightLeftHand(sleep = 2):
+  #pwm.setPWMFreq(60) 
+  pwm.setPWM(CHANNEL_LEFT_HAND, 0, SERVO_MIN_LEFT_HAND)
+  time.sleep(sleep)
+  pwm.setPWM(CHANNEL_LEFT_HAND, 0, SERVO_MAX_LEFT_HAND)
+  time.sleep(sleep)
+  pwm.setPWM(CHANNEL_LEFT_HAND, 0, SERVO_MIDDLE_LEFT_HAND)
+  time.sleep(sleep)
+
+#leftAndRightLeftHand()
+#time.sleep(sleep)
+upAndDownLeftArm()
 
 #for num in range(servoMin, servoMax):
 #  pulselen = ((num * (servoMax - servoMin)) / 180) + servoMin
 #  pwm.setPWM(0, 0, pulselen)
 
-while (True):
+#while (True):
   # Change speed of continuous servo on channel O
-  pwm.setPWM(0, 0, servoMin)
-  time.sleep(2)
-  pwm.setPWM(0, 0, servoMax)
-  time.sleep(2)
-  pwm.setPWM(0, 0, 1500)
-  time.sleep(2)
+  #upAndDownLeftArm()
+  #time.sleep(4)
+  #leftAndRightLeftHand()
+  #pwm.setPWM(1, 0, servoMin)
+  #time.sleep(2)
+  #pwm.setPWM(1, 0, servoMax)
+  #time.sleep(2)
+  #pwm.setPWM(1, 0, servoMiddle)
+  #time.sleep(2)
 
 
 
